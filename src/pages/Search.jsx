f6b27fc1a86ee6d60287ab2f7cc9f83f3779a140
João H <joaoheldermartins@hotmail.com>
2022-08-30 19:42:10 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    // loading: false,
+    // redirect: false,
+    userName: '',
+    isloginButtonDisabled: true,
+  };
+
+  validation = () => {
+    const { userName } = this.state;
+    const minCaracteres = 2;
+    if (userName.length >= minCaracteres) {
+      this.setState({ isloginButtonDisabled: false });
+    } else {
+      this.setState({ isloginButtonDisabled: true });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => { this.validation(); });
+  };
+
   render() {
+    const { userName, isloginButtonDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
@@ -10,14 +37,17 @@ class Search extends Component {
           <form>
             <input
               type="text"
-              name=""
+              name="userName"
               id="search-input"
               placeholder="Nome do artista"
               data-testid="search-artist-input"
+              value={ userName }
+              onChange={ this.handleChange }
             />
             <button
               type="button"
               data-testid="search-artist-button"
+              disabled={ isloginButtonDisabled }
             >
               Pesquisar
             </button>
